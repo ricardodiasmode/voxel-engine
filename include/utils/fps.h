b2ac94435e5a53a7db5f41fcbe4fbe96fe1c49ec
Ricardo Avelar <ricardodiasmode@hotmail.com>
2025-02-20 20:36:47 -0300

    creating some cpps

--- include/utils/fps.h
+++ include/utils/fps.h
@@ -1,44 +1,15 @@
 #include <iostream>
 #include <chrono>
 #include "utils/debug.h"
+#include "opengl/shaders/shader.h"
+
+class Text;
 
 namespace fps
 {
     static std::unique_ptr<Shader> textShader = nullptr;
 
-    float calculateFPS() {
-        static auto lastTime = std::chrono::high_resolution_clock::now();
+    float calculateFPS();
 
-        auto currentTime = std::chrono::high_resolution_clock::now();
-        float elapsedTime = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastTime).count();
-
-        lastTime = currentTime; // Update lastTime for the next frame
-
-        if (elapsedTime < 0.001f) { // Prevent division by near-zero (e.g., < 1 microsecond)
-            elapsedTime = 0.001f;   // Cap at 1000 FPS
-        }
-
-        return 1000.f / elapsedTime; // FPS = 1000 ms / frame time in ms
-    }
-
-    void drawFPS()
-    {
-        if (!textShader)
-        {
-            print("initializing text shader");
-            textShader = std::make_unique<Shader>("shaders/textVertex.glsl", "shaders/textFragment.glsl", "TextShader"); // Load your shaders
-            textShader->use();
-            textShader->setInt("text", 0); // Texture unit 0
-            glm::mat4 projection = glm::ortho(0.0f, 800.f, 0.0f, 600.f);
-            textShader->setMat4("projection", projection);
-        }
-
-        float fps = calculateFPS();
-        std::string fpsText = "FPS: " + std::to_string((int)fps);
-        glDisable(GL_DEPTH_TEST);
-        glDepthMask(GL_FALSE);
-        text::renderText(*textShader, fpsText, 10.0f, 560.0f, 1.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-        glDepthMask(GL_TRUE);
-        glEnable(GL_DEPTH_TEST);
-    }
+    void drawFPS(Text text);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,22 @@
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <filesystem>
 #include "utils/glhandler.h"
 #include "utils/text.h"
 #include "utils/fps.h"
-#include "opengl/shaders/shader.h"
 
 int main()
 {
-
     if (!glHandler::initialize())
         return -1;
 
     // Load font and set up text rendering
-    text::loadFont("resources/fonts/arial/ARIAL.ttf");  // Change to a real font path
-    text::initTextRendering();
+    Text fpsText = Text("resources/fonts/arial/ARIAL.ttf");  // Change to a real font path
 
     // Render Loop
     while (!glfwWindowShouldClose(glHandler::window))
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        fps::drawFPS();
+        fps::drawFPS(fpsText);
 
         // Swap buffers and poll events
         glfwSwapBuffers(glHandler::window);
